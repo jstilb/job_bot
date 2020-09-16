285c10d67d4448ca8eb2e0f651278ad65342652d
JM Snow <jstilb7@gmail.com>
2020-09-16 17:43:35 -0600

    clean and refactor

--- job_bot.py
+++ job_bot.py
@@ -2,9 +2,8 @@ from selenium import webdriver
 from time import sleep
 import csv
 
-job_title_to_search = 'Data Analyst'
+job_title_to_search = 'Tableau Analyst'
 location_to_search = 'Denver, Colorado'
-
 
 
 class JobBot():
@@ -21,10 +20,12 @@ class JobBot():
         self.driver.get('https://www.linkedin.com/jobs')
         sleep(5)
 
-
-
     def search(self):
+        # fills out search for desired job and location indicated in lines 5 & 6
+
         sleep(1)
+
+        # variables
         job_title_input = self.driver.find_element_by_xpath('//*[@id="JOBS"]/section[1]/input')
         location_input = self.driver.find_element_by_xpath('//*[@id="JOBS"]/section[2]/input')
         exit_autocomplete = self.driver.find_element_by_xpath('/html/body/main/section[1]')
@@ -32,14 +33,18 @@ class JobBot():
 
         job_title_input.send_keys(job_title_to_search)
         sleep(3)
+
         location_input.clear()
         location_input.send_keys(location_to_search)
         sleep(2)
+
         exit_autocomplete.click()
         sleep(2)
         search_btn.click()
 
     def scrape_web_links(self):
+
+        # scrolls down page to load all results
         SCROLL_PAUSE_TIME = 3
 
         # Get scroll height
@@ -58,26 +63,30 @@ class JobBot():
                 break
             last_height = new_height
 
+        # collect all job links in search result
         links = self.driver.find_elements_by_xpath("//*[@href]")
         clean_links = [x.get_attribute('href') for x in links if "jobs/view" in x.get_attribute('href')]
         jobs_info = []
 
+        # searches each job link and scrapes information
         for link in clean_links:
             try:
                 # pull up webpage & full job description
                 self.driver.get(str(link))
-                show_more_btn = self.driver.find_element_by_xpath('/html/body/main/section[1]/section[3]/div/section/button[1]')
+                show_more_btn = self.driver.find_element_by_xpath(
+                    '/html/body/main/section[1]/section[3]/div/section/button[1]')
                 show_more_btn.click()
                 sleep(2)
 
                 # scrape job info
-                job_title = self.driver.find_element_by_xpath('/html/body/main/section[1]/section[2]/div[1]/div[1]/a/h2')
+                job_title = self.driver.find_element_by_xpath(
+                    '/html/body/main/section[1]/section[2]/div[1]/div[1]/a/h2')
                 data = self.driver.find_element_by_class_name('description')
                 items = data.find_elements_by_tag_name('li')
 
                 # add items to list
                 job_info = []
-                items.insert(0, job_title) # to ensure job title will be the column header once we get it into a csv
+                items.insert(0, job_title)  # to ensure job title will be the column header once we get it into a csv
 
                 for item in items:
                     job_info.append(item.text)
@@ -86,18 +95,10 @@ class JobBot():
             except:
                 pass
 
-
         # download data
         with open(job_title_to_search + '.csv', 'w', newline='') as f:
             writer = csv.writer(f)
             writer.writerows(jobs_info)
-
-
-
-
-
-
-
 
 
 bot = JobBot()
