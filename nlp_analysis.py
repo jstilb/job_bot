c649740cb66cba07920044415791913d796b9f9a
JM Snow <jstilb7@gmail.com>
2020-10-02 14:48:30 -0600

    refactor and add save file as text function

--- nlp_analysis.py
+++ nlp_analysis.py
@@ -6,37 +6,33 @@ from nltk.util import ngrams
 import collections
 import heapq
 import gzip
-import csv
 
 # inputs
 file = 'Business Intelligence Analyst.txt.gz'  # file to be analyzed (should be in project folder)
-n = 20   # how many desired results you want in your bag of words model
-g = 5    # desired number of grams for n-grams model
+n = 20  # how many desired results you want in your bag of words model Ignore for ngrams
+g = 5  # desired number of grams for n-grams model
 l = 200  # limit for n-grams results
 save_file_as = "BI_Analyst_Results.txt"
-save_location = '/Users/jmsitunes/Desktop/projects/job_bot/' # where you plan to save results
 stop_words_input = ['a', 'u', 'at', 'year', 'le', 'etc', 'requirement', 'accredited', 'college', 'university',
                     'organization', 'employer', 'law', 'disability', 'accommodation', 'protected', 'race', 'color',
                     'work', 'opportunity', 'status', 'world', 'position', 'service', 'action', 'sexual', 'gender',
-                    'veteran', 'sex', 'orientation', 'age', 'national', 'origin', 'seniority', 'employment', 'applicant',
-                    'level', 'developmentsales', 'servicescomputer', 'associate', 'function', 'job', 'softwarefinancial',
-                    'religion']
-# 'a', 'u', 'at', 'year', 'le', 'etc', 'required', 'requirement', 'experience', 'skill', 'solution',
-#                     'business', 'organization', 'employer', 'application', 'environment', 'system', 'law', 'preferred',
-#                     'work', 'opportunity', 'status', 'world', 'role', 'position', 'service', 'action'
-
-
+                    'veteran', 'sex', 'orientation', 'age', 'national', 'origin', 'seniority', 'employment',
+                    'applicant',
+                    'level', 'developmentsales', 'servicescomputer', 'associate', 'function', 'job',
+                    'softwarefinancial',
+                    'religion']  # these are words you want to remove from set before model runs
 
 # open scraped job data
 with gzip.open(file, 'rb') as f:
-    data = f.read().decode('utf-8')
+    file_data = f.read().decode('utf-8')
 
 # sentence and work tokenization & normalization
 lemmatizer = WordNetLemmatizer()
 stop_words = set(stopwords.words('english'))  # remove general stop words
 final_stop_words = stop_words.union(stop_words_input)  # remove input stop words
 
-tokens = [word_tokenize(sen) for sen in sent_tokenize(data)]
+tokens = [word_tokenize(sen) for sen in sent_tokenize(file_data)]
+
 
 def normalize(tkns):
     global normalized_tokens
@@ -61,11 +57,10 @@ def tag(phrases):
         tags.append(pos_tag(phrase))
 
 
-# get word frequency (bag of words)
-bow = {}
-
-
+# gets count of each word for bag of words
 def get_bow_count(phrases):
+    global bow
+    bow = {}
     for phrase in phrases:
         for word in phrase:
             if word not in bow.keys():
@@ -74,16 +69,20 @@ def get_bow_count(phrases):
             bow[word] += 1
 
 
+# sorting for bag of words
 def sort_key(x):
     return -x[1], x[0]
 
 
+# function to call for bag of words model
 def bag_of_words(x):
     normalize(tokens)
     get_bow_count(normalized_tokens)
     top_n_results = heapq.nsmallest(x, bow.items(), key=sort_key)
     print(top_n_results)
 
+
+# n-grams model
 def extract_ngrams(data, grms, limit):
     normalize(data)
     extracted_ngrams = []
@@ -94,10 +93,12 @@ def extract_ngrams(data, grms, limit):
     results = collections.Counter(extracted_ngrams).most_common()[:limit]
     print(results)
 
-# def save_results(location, save_file, limit):
-#     extract_ngrams(tokens, g, l)
-#     with csv.open(location + save_file, 'wb') as f:
-#         f.write(results[:limit])
-#
-# save_results(save_location, save_file_as, l)
-# extract_ngrams(tokens, g, l)
+
+# save n-grams result to text file
+def save_results(save_file):
+    extract_ngrams(tokens, g, l)
+    new_file = open(save_file, 'w')
+    new_file.write(str(results))
+
+
+save_results(save_file_as)
